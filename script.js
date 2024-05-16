ee102b9e84b0ae0d73123958e3f4e5513cd31aff
causeway-rms <118050291+causeway-rms@users.noreply.github.com>
2024-05-16 09:16:36 +0530

    Add a function to find the winner

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const cells = [0,1,2,3,4,5,6,7,8];
 const values = [0,0,0,0,0,0,0,0,0];
-const startGameBtn = document.querySelector("#start-game");
+const winningCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 const playerXValue = 10;
 const playerOValue = 100;
+const startGameBtn = document.querySelector("#start-game");
+
 startGameBtn.addEventListener('click', startGame);
 
 function startGame() {
@@ -15,12 +17,6 @@ function displayBoard() {
     console.log(`${cells[0]} - ${cells[1]} - ${cells[2]}`);
     console.log(`${cells[3]} - ${cells[4]} - ${cells[5]}`);
     console.log(`${cells[6]} - ${cells[7]} - ${cells[8]}`);
-}
-
-function displayValues() {
-    console.log(`${values[0]} - ${values[1]} - ${values[2]}`);
-    console.log(`${values[3]} - ${values[4]} - ${values[5]}`);
-    console.log(`${values[6]} - ${values[7]} - ${values[8]}`);
 }
 
 function playGame() {
@@ -37,8 +33,8 @@ function getPlayerXChoice() {
     displayBoard();
     console.log(" ");
     values[playerXChoice] = playerXValue;
-    displayValues();
     console.log(" ");
+    findWinner();
 }
 
 function getPlayerOChoice() {
@@ -47,6 +43,30 @@ function getPlayerOChoice() {
     displayBoard();
     console.log(" ");
     values[playerOChoice] = playerOValue;
-    displayValues();
     console.log(" ");
+    findWinner();
+}
+
+function findWinner() {
+    let winningCombinationsItem = [];
+    let winningCombinationSum = 0;
+
+    for (let i = 0; i < winningCombinations.length; i++) {
+        winningCombinationsItem = winningCombinations[i];
+        for (let j = 0; j < winningCombinationsItem.length; j++) {
+            const winningCombinationCell = winningCombinationsItem[j];
+            winningCombinationSum += values[winningCombinationCell];
+        }
+        if (winningCombinationSum == 30) {
+            console.log("Player X Wins!");
+            console.log("Winning Combination: " + winningCombinationsItem);
+        }
+        else if (winningCombinationSum == 300) {
+            console.log("Player O Wins!")
+            console.log("Winning Combination: " + winningCombinationsItem);
+        }
+        else {
+            winningCombinationSum = 0;
+        }
+    }
 }
